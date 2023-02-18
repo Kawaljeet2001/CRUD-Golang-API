a881269631517017f490746829162f2d7dcab304
Kawaljeet2001 <batrakawaljeetsingh@gmail.com>
2023-02-18 14:42:01 +0530

    feat:added mux router

--- main.go
+++ main.go
@@ -6,29 +6,28 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"time"
 
 	"github.com/Kawaljeet2001/netflix-api/database"
-	"github.com/gorilla/mux"
+	"github.com/Kawaljeet2001/netflix-api/router"
 )
 
 func main() {
 	fmt.Println("This is a proper Backend API")
 	// port := os.Getenv("PORT")
 	port := 4000
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	go func() {
-		database.DBInstance(ctx)
+		database.DBInstance(context.Background())
 	}()
 
-	defer func() {
-		cancel()
-	}()
+	// defer func() {
+	// 	cancel()
+	// }()
 
 	//register the routes here
-	router := mux.NewRouter()
+	myrouter := router.CreateRouter()
 
 	//connect to the port here
 	log.Println("The server is up and runnning at port: ", port)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), router))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), myrouter))
 }
--- router/router.go
+++ router/router.go
@@ -1,1 +1,20 @@
 package router
+
+import (
+	"github.com/Kawaljeet2001/netflix-api/controller"
+	"github.com/gorilla/mux"
+)
+
+func CreateRouter() *mux.Router {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", controller.ServeHomeController).Methods("GET")
+	router.HandleFunc("/movies", controller.GetAllMoviesController).Methods("GET")
+	router.HandleFunc("/movie", controller.CreateMovieController).Methods("POST")
+	router.HandleFunc("/movie/{movieId}", controller.SetMovieAsWatchedController).Methods("PUT")
+	router.HandleFunc("/movie/{movieId}", controller.DeleteMovieByIdController).Methods("DELETE")
+	router.HandleFunc("/moviesd", controller.DeleteAllMoviesController).Methods("DELETE")
+
+	return router
+}
